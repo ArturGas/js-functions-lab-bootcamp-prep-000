7c56986e7f6281c2f951fc8b487ef77911e59bc1
ARTUR GASPARYAN <a.gasparyan.seua@gmail.com>
2018-09-03 15:17:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
-function sayhappyHolidays() {
-  console.log('Happy holidays!')
+function happyHollidays() {
+  consolee.log(Happy holidyays!)
 }
+happyHollidays(Happy holidays)
